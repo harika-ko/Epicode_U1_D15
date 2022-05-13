7820a589f822ffc4f489a7d485b9311c3bb9be5c
Harika_Kommuri <harikakommuri35@gmail.com>
2022-05-13 14:09:25 +0200

    Solved EXTRAS 41-43

--- index.js
+++ index.js
@@ -23,8 +23,22 @@ DOM EXERCISES
 38) Console log "Page loaded" when the page is correctly loaded
 39) Write a function to add new items to a unordered list
 40) Write a function to empty a list
-*/
 
+
+EXTRA EXERCISES
+
+41) Add an eventListener to show an alert when the cursor hovers a link, displaying its href property
+42) Create a button that will hide every image on the page when clicked
+43) Create a button that will hide or show the table on the page when clicked
+44) Write a function for calculating the sum of every number inside all the table cells (if their content is numeric)
+45) Delete the last letter from the heading each time the user clicks on it
+46) Change the background color of a <td> if the user clicks on it
+47) Add a delete button at the bottom of the table, when clicked it should delete a random <td>
+48) Add automatically a pink border to a cell when the mouse hovers it
+49) Write a function to create a table with 4 rows and 3 columns programmatically and add it to the bottom of the page
+50) Write a function to remove the last table from the page
+
+*/
 
 
 //21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
@@ -201,6 +215,32 @@ function emptyList() {
     }
 }
 
+//41) Add an eventListener to show an alert when the cursor hovers a link, displaying its href property
+
+function displayHref() {
+    let linksNode = document.getElementsByTagName("a")
+    for (let i = 0; i < linksNode.length; i++) {
+        linksNode[i].addEventListener("mouseover", function (e) {
+            alert(linksNode[i].href)
+        })
+    }
+}
+
+//42) Create a button that will hide every image on the page when clicked
+
+function hideImages() {
+    let imagesNode = document.getElementsByTagName("img")
+    for (let i = 0; i < imagesNode.length; i++) {
+        imagesNode[i].classList.add("visibility")
+    }
+}
+
+//43) Create a button that will hide or show the table on the page when clicked
+
+function showOrHideTable() {
+    let tableNode = document.getElementById("table")
+    tableNode.classList.toggle("visibility")
+}
 
 
 
